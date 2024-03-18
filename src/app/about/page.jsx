1ec15875820909a196ff7ba872a4ae46577ea0c3
Aondohemba <akighirjoseph77@gmail.com>
2024-03-18 14:35:28 +0100

    create an about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,14 @@
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+    <h1 className="text-2xl font-medium text-amber-600">About</h1>
+
+    <p>Welcome to your one-stop shop for everything movies! Whether you are a seasoned cinephile or a casual film fan, we have got something for you. Dive into our extensive database of movies, brimming with details on every film imaginable. From classic black and white masterpieces to the latest blockbusters, explore genres, directors, actors, and more.</p>
+
+    <p>Refine your search with powerful filters to find exactly what you are craving. Want a hilarious comedy to unwind with after a long day? Or perhaps a thought-provoking documentary to spark a conversation? Use our user-friendly tools to discover hidden gems or revisit old favorites.</p>
+
+    <p> So grab some popcorn, settle in, and get ready to embark on a cinematic adventure. We are here to make your movie exploration journey a truly enriching experience!</p>
+
+    </div>
+  )
+}
