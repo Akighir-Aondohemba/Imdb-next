56322b1983dd2d5e6df839e80e85a25ead1ada06
Aondohemba <akighirjoseph77@gmail.com>
2024-03-19 14:48:47 +0100

    fetch data from tmdb api in the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,21 @@
-export default function Home() {
-  return (
-    <div>Home</div>
-  )
+const API_KEY = process.env.API_KEY;
+import Results from "./components/Results";
+
+export default async function Home({searchParams}) {
+  const genre =searchParams.genre || 'fetchTrending';
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}&language-US&page=1`
+      );
+    const data = await res.json();
+    if(!res.ok){
+      throw new Error('Failed to fetch data')
+    }
+    const results = data.results;
+    
+  
+    return <div>
+      <Results results={results} />
+    </div>;
+  
 }
